786d8cb3aff9893dc64442b4185f3ab740c9f831
xavierpok <54708640+xavierpok@users.noreply.github.com>
2023-10-11 22:23:48 +0800

    Create LastModifiedDateTime Class

---
+++ src/main/java/seedu/address/model/person/LastModifiedDateTime.java
@@ -0,0 +1,43 @@
+package seedu.address.model.person;
+
+import java.time.LocalDateTime;
+
+import static java.util.Objects.requireNonNull;
+
+public class LastModifiedDateTime {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Last modified should consist of a DateTime Object, and creation not exposed to user.";
+
+    private LocalDateTime lastModified;
+
+    public LastModifiedDateTime(LocalDateTime lastModified) {
+        requireNonNull(lastModified);
+        this.lastModified = lastModified;
+        // no other sanity checking should be required, as the
+        // LocalDateTime object encapsulates already.
+        // No sanity checking on the SEMANTICS of the provided LocalDateTime is provided.
+        // Doing so goes beyond the responsibilities of this class.
+    }
+
+    @Override
+    public String toString() {
+        return lastModified.toString();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (!(other instanceof LastModifiedDateTime)) {
+            return false;
+        }
+        // safe as we did type validation above
+        LastModifiedDateTime otherLMDT = (LastModifiedDateTime) other;
+        return lastModified.equals(otherLMDT.lastModified);
+    }
+
+    //TODO : Hashing. Hashing is not recommended for LocalDateTime instances.
+
+}
