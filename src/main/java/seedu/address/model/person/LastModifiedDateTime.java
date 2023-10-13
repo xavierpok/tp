8371fc7f209af5df479be29450941178b384c79d
xavierpok <54708640+xavierpok@users.noreply.github.com>
2023-10-13 21:39:45 +0800

    Add valid checker method in LastModifiedDateTime

--- src/main/java/seedu/address/model/person/LastModifiedDateTime.java
+++ src/main/java/seedu/address/model/person/LastModifiedDateTime.java
@@ -1,11 +1,17 @@
 package seedu.address.model.person;
 
+import static java.util.Objects.isNull;
 import static java.util.Objects.requireNonNull;
 
 import java.text.DateFormat;
+import java.text.ParsePosition;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.format.FormatStyle;
+import java.time.format.ResolverStyle;
+import java.time.temporal.TemporalAccessor;
+import java.util.regex.Pattern;
 
 
 /**
@@ -22,7 +28,12 @@ public class LastModifiedDateTime {
      */
     public static final LocalDateTime DEFAULT_LAST_MODIFIED = LocalDateTime.of(
             10, 10, 10, 10, 10);
+
+    public static final DateTimeFormatter LASTMODIFIED_FORMATTER =
+            DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).withResolverStyle(ResolverStyle.STRICT);
     private LocalDateTime lastModified;
+
+
 
     /**
      * Constructs a @code LastModifiedDateTime
@@ -35,6 +46,18 @@ public class LastModifiedDateTime {
         // LocalDateTime object encapsulates already.
         // No sanity checking on the SEMANTICS of the provided LocalDateTime is provided.
         // Doing so goes beyond the responsibilities of this class.
+    }
+
+    /**
+     * Returns if a string representation of a date & time is valid (matching the format of this class)
+     */
+    public static boolean isValidLastModifiedDateTime(String lastModified) {
+
+        ParsePosition parsePosition = new ParsePosition(0);
+        TemporalAccessor result = LASTMODIFIED_FORMATTER.parseUnresolved(lastModified,parsePosition);
+        return (isNull(result));
+        // parseUnresolved returns null if some error is occurred in parsing
+        // If some error is run into, then this implies that it's not a valid LastModifiedDateTime.
     }
 
     @Override
