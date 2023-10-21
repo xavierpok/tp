e8845d27cc6a0cdc2a3c7984c285c81d1aa6ba73
xavierpok <54708640+xavierpok@users.noreply.github.com>
2023-10-21 13:56:16 +0800

    Add javadoc to new interfaces

--- src/main/java/connexion/model/person/PersonDetailField.java
+++ src/main/java/connexion/model/person/PersonDetailField.java
@@ -1,6 +1,17 @@
 package connexion.model.person;
 
+
+/**
+ * API for fields in general that are associated with Person, with a defined method for retrieving the string
+ * representation for view in a more verbose context.
+ * For some fields, they may have only representations in a more verbose context as they are meant to only be seen
+ * in a more verbose context.
+ * @param <T> the datatype this field wraps around
+ */
 public interface PersonDetailField<T> extends PersonField<T>{
 
+    /**
+     * Returns the user-facing string representation of this field in a detail view.
+     */
     public String getDetailString();
 }
