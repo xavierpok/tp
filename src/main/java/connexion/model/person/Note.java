25601859fb2e4ea26bc2c1c8fec7efc1582aa52f
imkwokyong <darrenkuan@gmail.com>
2023-10-28 21:35:45 +0800

    Add Note

---
+++ src/main/java/connexion/model/person/Note.java
@@ -0,0 +1,72 @@
+package connexion.model.person;
+
+import static connexion.commons.util.AppUtil.checkArgument;
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents a Person's note in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidNote(String)}
+ */
+public class Note implements PersonDetailField<String> {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Notes should only contain alphanumeric characters and spaces, can be blank";
+
+    // regex to be changed
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
+    private final String note;
+
+    /**
+     * Constructs a {@code Note}.
+     *
+     * @param note A valid note.
+     */
+    public Note(String note) {
+        requireNonNull(note);
+        checkArgument(isValidNote(note), MESSAGE_CONSTRAINTS);
+        this.note = note;
+    }
+
+    @Override
+    public String toString() {
+        return note;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Note)) {
+            return false;
+        }
+
+        Note otherNote = (Note) other;
+        return note.equals(otherNote.note);
+    }
+
+    @Override
+    public int hashCode() {
+        return note.hashCode();
+    }
+
+    /**
+     * Returns true if a given string is a valid note.
+     */
+    public static boolean isValidNote(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String getDetailString() {
+        return "Note: " + note;
+    }
+
+    @Override
+    public String getValue() {
+        return note;
+    }
+}
