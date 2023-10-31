403ab7aeecfcad78b4794a0e912bf185908240b9
imkwokyong <darrenkuan@gmail.com>
2023-10-31 18:37:02 +0800

    Update conflicting AddCommandTest

--- src/test/java/connexion/logic/commands/AddCommandTest.java
+++ src/test/java/connexion/logic/commands/AddCommandTest.java
@@ -152,6 +152,16 @@ public class AddCommandTest {
         }
 
         @Override
+        public void setDetailedPerson(Person person) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Person getDetailedPerson() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public boolean hasPerson(Person person) {
             throw new AssertionError("This method should not be called.");
         }
