64cb540bf4ba1dd2c29f7f041e0a246cc2bdd59b
xavierpok <54708640+xavierpok@users.noreply.github.com>
2023-11-11 18:25:43 +0800

    Edit error msgs to accurate restrictions on INDEX

--- src/main/java/connexion/logic/commands/MarkCommand.java
+++ src/main/java/connexion/logic/commands/MarkCommand.java
@@ -19,7 +19,7 @@ public class MarkCommand extends Command {
     public static final String COMMAND_WORD = "mark";
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Marks the person identified by the index number used in the displayed person list.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Parameters: INDEX (must be a positive integer and less than 2^31 - 1)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_MARK_SUCCESS = "Marked Person: %1$s";
